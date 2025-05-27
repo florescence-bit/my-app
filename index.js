ffdd91b44236dbbf2cc0802b10ff5546126e539c
san <adityarajsharma.021@gmail.com>
2025-05-27 12:03:04 +0530

    version 3.0

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const PORT = process.env.PORT || 3000;
 
 // Basic route
 app.get('/', (req, res) => {
-	  res.send('Hello from Node.js!');
-	  });
-
-	  // Start server
-	  app.listen(PORT, () => {
-	  	  console.log(`Server running at http://localhost:${PORT}`);
-	  	  });
-	  	  
-	  })
-})
+    res.send('Hello from Node.js! new <a herf="/home">home</a>');
+});
+app.get('/home', (req, res) => {
+    res.send('u are at home 💩');
+ });
+// Start server
+app.listen(PORT, () => {
+	console.log(`Server running at http://localhost:${PORT}`);
+});
